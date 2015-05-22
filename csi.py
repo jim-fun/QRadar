5d04d92640f72424d14f36b0e85b873fdd99e2c9
J. Meyer <jmeyer@intelpc.com>
2015-05-21 21:23:52 -0400

    first commit

---
+++ csi.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+# 5/21/2015 - J. Meyer
+import csv, urllib, urllib2, json
+from IPy import IP
+
+path_to_ca_pem = 'The CA PEM for your qradar SIEM'
+qradar_fqdn = 'hostname.example.com'
+qradar_api = 'https://' + qradar_fqdn + '/api/reference_data/sets/bulk_load/csi_'
+api_key = 'your api key'
+file_import = '/opt/critical-stack/frameworks/intel/master-public.bro.dat'
+file_out_dir = '/var/www/lists/'
+file_export = file_out_dir + 'csi_'
+types = {'addr':[],'domain':[],'url':[],'file_hash':[],'indicator':[]}
+
+with open(file_import, 'rU') as file1:
+	reader = csv.reader(file1, dialect="excel-tab")
+	for row in reader:
+		try:
+			row_name = row[1].lower()
+			row_name = row_name.replace('intel::','')
+			if row_name =='addr':
+				IP(row[0])
+			types[row_name].append(row)
+		except:
+			pass
+
+for x in types.keys():
+	if not 'indicator' in x:
+		a = []
+		i = 0
+		z = 0
+		for y in types[x]:
+			a.append(y[0])
+		b = list(set(a))
+		b.sort()
+		url = qradar_api + x
+		lb = len(b)								# length of b
+		hmb = lb/5000								# how many times b can be devided by 5000
+		while (z <= hmb):
+			i1 = z*5000
+			i2 = z*5000+5000
+			req = urllib2.Request(url)
+			req.add_header('SEC', api_key)
+			req.add_header('Content-Type', 'application/json')
+			req.add_data(json.dumps(b[i1:i2]))
+			try:
+				response = urllib2.urlopen(req,cafile=path_to_ca_pem).read()
+			except:
+				print "fail"
+				raise
+				pass
+			z+=1
+
+with open(file_export+'all', 'wb') as file2:
+	out_put = csv.writer(file2, delimiter='\t', quoting=csv.QUOTE_NONE)
+	out_put.writerow(['Indicator','Indicator Type','Source','Do Notice'])
+	for x in types.keys():
+	        if not 'indicator' in x:
+			for y in types[x]:
+				out_put.writerow(y)
+ 
---
+++ seculert_crime_servers.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+# J. Meyer - 5/21/2015
+
+#imports
+import csv, urllib, urllib2, json
+from IPy import IP
+
+#variables
+qradar_ca_pem = 'path to CA PEM for QRadar'
+qradar_fqdn = 'hostname.example.com'
+seculert_key = "insert your seculert key"
+url="https://api.seculert.com/CrimeServers?api_key=" + seculert_key
+qradar_api = 'https://' + qradar_fqdn + '/api/reference_data/sets/bulk_load/seculert_'
+api_key = 'QRadar API key'
+file_out_dir = '/var/www/lists/'
+types = {'urls':[],'ips':[]}
+
+#pull data
+data = json.load(urllib2.urlopen(url))
+
+#create full file
+with open(file_out_dir+"seculert.csv", "wb") as file:
+	csv_file = csv.writer(file)
+	csv_file.writerow(["threat-type-id","threat-type-name","url","name","ip-address","first-seen","last-seen"])
+	for item in data['crime-servers']:
+		csv_file.writerow([item['threat-type-id'],item['threat-type-name'],item['url'],item['name'],item['ip-address'],item['first-seen'],item['last-seen']])
+
+#prepare data for reference set
+for item in data['crime-servers']:
+	if item['url'] is not None:
+		types['urls'].append([item['url']])
+	if item['ip-address'] is not None:
+		types['ips'].append([item['ip-address']])
+
+for x in types.keys():
+	a = []
+	i = 0
+	z = 0
+	for y in types[x]:
+		a.append(y[0])
+	b = list(set(a))
+	b.sort()
+	url = qradar_api + x
+	lb = len(b)								# length of b
+	hmb = lb/5000								# how many times b can be devided by 5000
+	while (z <= hmb):
+		i1 = z*5000
+		i2 = z*5000+5000
+		req = urllib2.Request(url)
+		req.add_header('SEC', api_key)
+		req.add_header('Content-Type', 'application/json')
+		req.add_data(json.dumps(b[i1:i2]))
+		try:
+			response = urllib2.urlopen(req,cafile=qradar_ca_pem).read()
+		except:
+			print "failed"
+			raise
+			pass
+		z+=1
